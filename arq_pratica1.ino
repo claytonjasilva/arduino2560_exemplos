8d96a206212a60929e94a55aa7e296529eaaa7f4
Clayton Silva <80921613+claytonjasilva@users.noreply.github.com>
2022-08-12 08:15:10 -0300

    Update arq_pratica1.ino

--- arq_pratica1.ino
+++ arq_pratica1.ino
@@ -4,18 +4,18 @@
 void setup() {
   // configura os pinos da placa
 
-  pinMode(1,OUTPUT); //Configura o pino 1 da placa como saída 
+  pinMode(10,OUTPUT); //Configura o pino 10 da placa como saída 
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  digitalWrite(1,HIGH); //Gera um sinal alto no pino 1 - configura o pino 'on'
+  digitalWrite(10,HIGH); //Gera um sinal alto no pino 10 - configura o pino 'on'
   delay(5000); //Pausa durante 5 seg
-  digitalWrite(1,LOW); //Gera um sinal baixo no pino 1 - configura o pino 'off'
+  digitalWrite(10,LOW); //Gera um sinal baixo no pino 10 - configura o pino 'off'
   delay(5000); //Pausa durante 5 seg
-  digitalWrite(1,HIGH); //Gera um sinal alto no pino 1 - configura o pino 'on'
+  digitalWrite(10,HIGH); //Gera um sinal alto no pino 10 - configura o pino 'on'
   delay(3000); //Pausa durante 3 seg
 
 }
